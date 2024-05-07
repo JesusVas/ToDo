6aa17979af5872219d89e9baec9aebadd999d6d2
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2024-05-06 21:28:40 -0400

    Testing some things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import ToDoCreate from "./components/ToDoCreate";
 import ToDoRecord from "./components/ToDoRecord";
+let i=0;
 const App=()=>{
+    const [tasks,setTasks]=useState([])
+    const createTask=(t)=>{
+        const updatedTask=[
+            ...tasks,{id:i++,name:t}
+        ]
+        setTasks(updatedTask);
+        console.log("the task is: "+t);
+    }
+    tasks.map((task)=>{
+        console.log(task);
+    })
     return(
         <div>
             App
-            <ToDoCreate/>
+            <ToDoCreate submitTask={createTask}/>
             <ToDoRecord/>
         </div>
     );
--- src/components/ToDoCreate.js
+++ src/components/ToDoCreate.js
@@ -1,7 +1,21 @@
-const ToDoCreate=()=>{
+import { useState } from "react";
+
+const ToDoCreate=({submitTask})=>{
+    const [name,setName]=useState("");
+    const handleChange=(e)=>{
+        setName(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        submitTask(name);
+        setName("");
+    }
     return(
         <div>
-            ToDoCreate
+            <form onSubmit={handleSubmit}>
+                <input value={name} onChange={handleChange}/>
+                <button>Agregar tarea</button>
+            </form>
         </div>
     );
 }
